966dc5db15f16d8ce55b046c871bb434af54a047
khikashika <omnidept@gmail.com>
2022-02-14 17:58:36 +0300

    library song getting

--- src/main/java/Songer/RPCObjects/Song.java
+++ src/main/java/Songer/RPCObjects/Song.java
@@ -1,7 +1,46 @@
 package Songer.RPCObjects;
 
 public class Song {
-    String label;
-    int id;
-    String artist;
+   private String label;
+   private int id;
+   private String artist;
+
+
+    public Song(String label, int id) {
+        this.label = label;
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "label='" + label + '\'' +
+                ", id=" + id +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
 }
--- src/main/java/Songer/SongController/SongController.java
+++ src/main/java/Songer/SongController/SongController.java
@@ -4,15 +4,14 @@ import Songer.ObjectToJsonString;
 import Songer.PostRequestUnirest;
 import Songer.RPCObjects.RpcFather;
 import Songer.RPCObjects.Song;
-import com.google.gson.*;
-import jdk.nashorn.internal.parser.JSONParser;
-import kong.unirest.Unirest;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
 //import static com.pengrad.telegrambot.BotUtils.gson;
 
@@ -29,21 +28,68 @@ public class SongController {
 //    }
 
     public String getSongsLibrary(){
+
+        //Getting library from kodi//
         RpcFather getLibrary = new RpcFather(1,"AudioLibrary.GetSongs",null);
         ObjectToJsonString obj = new ObjectToJsonString();
         String jsonString = obj.makeJsonString(getLibrary);
         PostRequestUnirest requestUnirest = new PostRequestUnirest();
         String response = requestUnirest.uniPost(System.getenv("KODI_URL"),jsonString);
+
+
+        //Map Songs and ids from String(JSON)response //
+
         JSONObject json = new JSONObject(response);
         JSONObject result = new JSONObject(json.get("result").toString());
         ArrayList<Song> songs = new ArrayList<Song>();
-        //ArrayList<String> rawSongs = new ArrayList<String>(Arrays.asList(toString().result.get("songs").);
-        for(String s :result.keySet()){
-            System.out.println(s+"="+result.get(s));
-            System.out.println(result.get(s).)
+        Object jsonSongs = result.get("songs");
+        String strJsonSongs = jsonSongs.toString();
+        strJsonSongs= strJsonSongs.replace("{","");
+        strJsonSongs= strJsonSongs.replace("[","");
+        strJsonSongs= strJsonSongs.replace("]","");
+        strJsonSongs= strJsonSongs.replace("\"","");
+        strJsonSongs= strJsonSongs.replace("},",";");
+        strJsonSongs= strJsonSongs.replace("}","");
+        ArrayList<String> rawSongs = new ArrayList<String>(Arrays.asList(strJsonSongs.split(";")));
+        ArrayList<Map> mapArrayList = new ArrayList<Map>();
+
+        for(int i=0 ; i<rawSongs.size(); i++)   {
+            String[] sub = rawSongs.get(i).split(",");
+
+            Map<String,String> map = new HashMap<>();
+            for (String subs:sub) {
+                String[] sub2 = subs.split(":"); //
+                map.put(sub2[0],sub2[1]);
+                }
+
+            mapArrayList.add(map);
+            }
+        for (int k = 0;k<mapArrayList.size();k++){
+         songs.add( new Song(mapArrayList.get(k).get("label").toString(),Integer.parseInt(mapArrayList.get(k).get("songid").toString())));
+        }
+
+        //Getting Artist
+
+        System.out.println("Getting song details");
+        for(Song song:songs){
+            HashMap<String,Integer> map = new HashMap<>();
+            map.put("songid",song.getId());
+            RpcFather getArtist = new RpcFather(1,"AudioLibrary.GetArtistDetails",map);
+            jsonString = obj.makeJsonString(getArtist);
+            response = requestUnirest.uniPost(System.getenv("KODI_URL"),jsonString);
+            System.out.println(response);
         }
 
 
+
+//**{
+//    "id":1,
+//    "jsonrpc":"2.0",
+//    "method":"AudioLibrary.GetSongs",
+//    "params":{
+//    "filter":{"field":"artist","operator":"is","value":"Chris Zabriskie"}
+//    }
+//    }**//
 
         //JsonObject result = new JsonObject(json.get("result"));
 
